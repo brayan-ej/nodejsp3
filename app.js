4a4a9ab202ead8567c2486b770d47b58eca85980
Brayan-ej <brayan.ej@outlook.com>
2020-10-31 17:51:07 -0700

    econd commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("Welcome to Brayan's test home page!!");
 });
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("Whatsapp");
 });
